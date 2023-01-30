31ce338ff13fc649be39e0c677212896b5a704fe
Simon Börner <boernersimon@googlemail.com>
2023-01-30 16:53:20 +0100

    change icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
     <div className="w-11/12 flex flex-col items-center">
-      <img src="/sofa2.svg" alt="sofa"></img>
-      <h1 className="text-3xl mb-16 font-extrabold">LEBENSLOTSE</h1>
+      <img src="/chaise.svg" alt="sofa"></img>
+      <h1 className="text-3xl mb-10 mt-7 font-extrabold">LEBENSLOTSE</h1>
     </div>
   );
 };
