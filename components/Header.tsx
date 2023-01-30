e1f8a83609f5280cb4970aadf7eff2ca9e70d8d8
Simon Börner <boernersimon@googlemail.com>
2023-01-30 19:38:29 +0100

    change swipe behaviour

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="w-11/12 flex flex-col items-center">
+    <div className="w-11/12 flex flex-col items-center mt-7">
       <img src="/chaise.svg" alt="sofa"></img>
       <h1 className="text-3xl mb-10 mt-7 font-extrabold">LEBENSLOTSE</h1>
     </div>
--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,5 +1,8 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectCreative } from "swiper";
 import "swiper/css";
+
+import "swiper/css/effect-creative";
 
 interface SliderProps {
   color: string;
@@ -9,8 +12,20 @@ interface SliderProps {
 const Slider = ({ text, color }: SliderProps) => {
   return (
     <Swiper
+      modules={[EffectCreative]}
+      effect="creative"
+      creativeEffect={{
+        prev: {
+          shadow: true,
+          translate: [0, 0, -400],
+        },
+        next: {
+          translate: ["100%", 0, 0],
+        },
+      }}
       spaceBetween={5}
       slidesPerView={1}
+      loop={true}
       onSlideChange={() => console.log("slide change")}
       onSwiper={(swiper) => console.log(swiper)}
     >
