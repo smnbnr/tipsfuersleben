e08dfdc07a747af1c12df18e964b64c8871dfd4a
Simon Börner <boernersimon@googlemail.com>
2023-02-10 17:47:36 +0100

    fix export slider

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({ text, href, onClick }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="border border-black border-dotted w-11/12 h-16 flex justify-center items-center gap-2 mt-9 bg-cyan-200"
+      className="border border-black border-dotted w-11/12 h-12 flex justify-center items-center gap-2 mt-2 bg-cyan-200"
     >
       {href ? <Link href={href}>{text}</Link> : text}
     </button>
