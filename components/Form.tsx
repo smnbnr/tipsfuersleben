fc3165645662cff73f9b5ed8e3095a4177e13267
Simon Börner <boernersimon@googlemail.com>
2023-02-17 17:51:03 +0100

    add visual feedback on submit

--- components/Form.tsx
+++ components/Form.tsx
@@ -21,13 +21,20 @@ const Form = ({
   const [userTopAdvice, setUserTopAdvice] = useState("");
   const [userMiddleAdvice, setUserMiddleAdvice] = useState("");
   const [userBottomAdvice, setUserBottomAdvice] = useState("");
-
+  const [addConfirmTop, setAddConfirmTop] = useState(false);
+  const [addConfirmMiddle, setAddConfirmMiddle] = useState(false);
+  const [addConfirmBottom, setAddConfirmBottom] = useState(false);
+  const resetSubmit = () => {
+    setAddConfirmTop(false);
+    setAddConfirmMiddle(false);
+    setAddConfirmBottom(false);
+  };
   return (
     <div className="flex flex-col w-11/12">
-      <div className="relative focus-within:z-10 focus-within:border-green-200 focus-within:ring-1 focus-within:ring-green-200">
+      <div className="relative">
         <label
           htmlFor="input1"
-          className="mt-3 block font-medium text-xl text-gray-900"
+          className="block font-medium text-xl text-gray-900"
         >
           ERSTER TEIL
         </label>
@@ -36,21 +43,24 @@ const Form = ({
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
             setUserTopAdvice(e.target.value)
           }
+          onFocus={resetSubmit}
           value={userTopAdvice}
           type="text"
           name="input1"
           id="input1"
-          className="block w-full border-0 p-1 py-1 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-          placeholder="Du solltest wirklich..."
+          className="block w-full border-0 p-1 py-2 text-gray-900 placeholder-gray-400 focus:ring-0 sm:text-sm"
+          placeholder="DU SOLLTEST WIRKLICH..."
         />
         <Submit
+          addConfirm={addConfirmTop}
           onClick={() => {
             onSetTopAdvices([...topAdvices, userTopAdvice]);
             setUserTopAdvice("");
+            setAddConfirmTop(true);
           }}
         />
       </div>
-      <div className="relative focus-within:z-10 focus-within:border-blue-200 focus-within:ring-1 focus-within:ring-blue-200">
+      <div className="relative">
         <label
           htmlFor="input2"
           className="mt-3 block font-medium text-gray-900 text-xl"
@@ -58,6 +68,7 @@ const Form = ({
           ZWEITER TEIL
         </label>
         <input
+          onFocus={resetSubmit}
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
             setUserMiddleAdvice(e.target.value)
           }
@@ -65,17 +76,19 @@ const Form = ({
           type="text"
           name="input2"
           id="input2"
-          className="block w-full border-0 p-1 py-1 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-          placeholder="genau hier..."
+          className="block w-full border-0 p-1 py-2 text-gray-900 placeholder-gray-400 focus:ring-0 sm:text-sm"
+          placeholder="...HIER"
         />
         <Submit
+          addConfirm={addConfirmMiddle}
           onClick={() => {
             onSetMiddleAdvices([...middleAdvices, userMiddleAdvice]);
             setUserMiddleAdvice("");
+            setAddConfirmMiddle(true);
           }}
         />
       </div>
-      <div className="relative focus-within:z-10 focus-within:border-red-200 focus-within:ring-1 focus-within:ring-red-200">
+      <div className="relative">
         <label
           htmlFor="job-title"
           className="mt-3 block font-medium text-gray-900 text-xl"
@@ -83,6 +96,7 @@ const Form = ({
           DRITTER TEIL
         </label>
         <input
+          onFocus={resetSubmit}
           onChange={(e: ChangeEvent<HTMLInputElement>) =>
             setUserBottomAdvice(e.target.value)
           }
@@ -90,13 +104,15 @@ const Form = ({
           type="text"
           name="input3"
           id="input3"
-          className="block w-full border-0 p-1 py-1 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm"
-          placeholder="was reinschreiben!"
+          className="block w-full border-0 p-1 py-2 text-gray-900 placeholder-gray-400 focus:ring-0 sm:text-sm"
+          placeholder="...WAS REINSCHREIBEN!"
         />
         <Submit
+          addConfirm={addConfirmBottom}
           onClick={() => {
             onSetBottomAdvices([...bottomAdvices, userBottomAdvice]);
             setUserBottomAdvice("");
+            setAddConfirmBottom(true);
           }}
         />
       </div>
--- components/Submit.tsx
+++ components/Submit.tsx
@@ -1,11 +1,19 @@
 type SubmitProps = {
   onClick: any;
+  addConfirm: boolean;
 };
 
-const Submit = ({ onClick }: SubmitProps) => {
+const Submit = ({ onClick, addConfirm }: SubmitProps) => {
   return (
-    <button onClick={onClick} className="my-2 px-1 py-1 bg-green-200 text-sm">
-      HINZUFÜGEN
+    <button
+      onClick={onClick}
+      className={
+        addConfirm
+          ? "my-2 px-3 py-1 bg-green-300 text-sm"
+          : "my-2 px-3 py-1 bg-white text-sm"
+      }
+    >
+      {addConfirm ? "HINZUGEFÜGT!" : "HINZUFÜGEN"}
     </button>
   );
 };
